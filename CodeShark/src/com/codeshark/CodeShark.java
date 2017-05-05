7360af1e8d7b107dd4d06474e39be2e107ea4942
e23934 <tupesudhirkumar@gmail.com>
2017-05-06 03:18:19 +0530

    added show api

--- CodeShark/src/com/codeshark/CodeShark.java
+++ CodeShark/src/com/codeshark/CodeShark.java
@@ -3,5 +3,10 @@ package com.codeshark;
 public class CodeShark {
 	public static void main(String[] args) {
 		System.out.println("Code Sharlk");
+		show();
+	}
+	
+	public static void show(){
+		System.out.println("Show");
 	}
 }
