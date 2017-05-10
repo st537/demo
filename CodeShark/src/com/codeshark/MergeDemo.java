6c6831d6f18aafbb0109b7365f60735d55a35c03
e23934 <tupesudhirkumar@gmail.com>
2017-05-10 23:24:51 +0530

    prove

--- CodeShark/src/com/codeshark/MergeDemo.java
+++ CodeShark/src/com/codeshark/MergeDemo.java
@@ -2,6 +2,6 @@ package com.codeshark;
 
 public class MergeDemo {
 public static void main(String[] args) {
-	System.out.println("You have to learn the git merge!");
+	System.out.println("You have to prove urself");
 }
 }
